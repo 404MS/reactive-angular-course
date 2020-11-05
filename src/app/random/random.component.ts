4c5c5f73827227dcfc9cd8748dcd057eca3828aa
404MS <miguelsl1912@gmail.com>
2020-11-05 16:10:22 -0500

    Quill JS testing

--- src/app/random/random.component.ts
+++ src/app/random/random.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 
 @Component({
@@ -9,10 +10,35 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 export class RandomComponent implements OnInit {
 
   public ckEditor = ClassicEditor;
+  editorForm: FormGroup;
+  content: string;
 
-  constructor() { }
+  editorStyle = {
+    height: '250px',
+    width: '800px',
+    backgroundColor: '#ffffff'
+  }
+
+  config = {
+    toolbar: [
+      ['bold', 'italic', 'underline'],
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      [{ 'align': [] }],
+      ['link', 'image', 'video'] 
+    ]
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.editorForm = this.fb.group({
+      editor: ['<p>Hola a todos</p>', []]
+    })
+  }
+
+  onSubmit(): void {
+    this.content = this.editorForm.get('editor').value;
+    console.log(this.content);
   }
 
 }
